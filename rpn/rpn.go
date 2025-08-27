2e0be99eb1ba5638b1d7863cdd0e0ecef062476a
Matt Wachowski <matt.wachowski@gmail.com>
2025-08-27 13:26:33 -0700

    implemented macro execution

--- rpn/rpn.go
+++ rpn/rpn.go
@@ -2,6 +2,7 @@ package rpn
 
 import (
 	"fmt"
+	"mattwach/rpngo/parse"
 	"strconv"
 	"strings"
 )
@@ -31,6 +32,9 @@ func (rpn *RPN) Exec(arg string) error {
 		}
 		if arg[0] == '$' {
 			return rpn.getVariable(arg[1:])
+		}
+		if arg[0] == '@' {
+			return rpn.execVariableAsMacro(arg[1:])
 		}
 	}
 	if len(arg) >= 2 {
@@ -66,6 +70,28 @@ func (rpn *RPN) getVariable(name string) error {
 		return fmt.Errorf("unknown variable: $%s", name)
 	}
 	return rpn.Stack.PushFrame(f)
+}
+
+// Executes a variables as a macro
+func (rpn *RPN) execVariableAsMacro(name string) error {
+	f, ok := rpn.variables[name]
+	if !ok {
+		return fmt.Errorf("unknown variable: @%s", name)
+	}
+	if f.Type == COMPLEX_FRAME {
+		// Just push the frame
+		return rpn.Stack.PushFrame(f)
+	}
+	fields, err := parse.Fields(f.Str)
+	if err != nil {
+		return err
+	}
+	for _, f := range fields {
+		if err := rpn.Exec(f); err != nil {
+			return fmt.Errorf("@%s(%s): %v", name, f, err)
+		}
+	}
+	return nil
 }
 
 // Pushes a float onto the stack
