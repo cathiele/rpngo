4e44040ae9ce7e0f0c6bdd700fe67486360ea41b
Matt Wachowski <matt.wachowski@gmail.com>
2025-09-01 17:33:07 -0700

    Split plotwin into 3 files.
    It was getting to be a big file.

---
+++ io/window/plotwin/axis.go
@@ -0,0 +1,210 @@
+package plotwin
+
+import (
+	"fmt"
+	"mattwach/rpngo/io/window"
+)
+
+func (pw *PlotWindow) drawAxis() error {
+	if err := pw.txtw.Color(31, 31, 31, 0, 0, 0); err != nil {
+		return err
+	}
+
+	x, xok := pw.transformX(0)
+	if xok {
+		if err := pw.drawVerticalAxis(x); err != nil {
+			return err
+		}
+	}
+
+	y, yok := pw.transformY(0)
+	if yok {
+		if err := pw.drawHorizontalAxis(y); err != nil {
+			return err
+		}
+	}
+
+	if xok && yok {
+		pw.txtw.SetXY(x, y)
+		if err := pw.txtw.Write('+'); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (pw *PlotWindow) drawVerticalAxis(x int) error {
+	h := pw.txtw.Height()
+	for y := 0; y < h; y++ {
+		pw.txtw.SetXY(x, y)
+		if err := pw.txtw.Write('|'); err != nil {
+			return err
+		}
+	}
+	pw.drawVerticalTickMarks(x)
+	return nil
+}
+
+// About axis drawing
+// the goal is to make it look nice while also being useful
+// We are going for rounded tick marks (e.g. 0.25, 1.0, ...)
+// and reasonable pixels per mark
+
+const minVerticalSpacing = 5.0
+const maxVerticalSpacing = 10.0
+
+func (pw *PlotWindow) drawVerticalTickMarks(wx int) {
+	wh := pw.txtw.Height()  // height in characters
+	yr := pw.maxy - pw.miny // units
+	cpu := float64(wh) / yr // characters / unit
+	var te float64 = 1      // ticks every (0.5, 1, etc)
+	if cpu > maxVerticalSpacing {
+		te = searchScaleDownward(cpu, minVerticalSpacing)
+	} else if cpu < minVerticalSpacing {
+		te = searchScaleUpward(cpu, maxVerticalSpacing)
+	}
+
+	// becuase te was carefully selected to provide a limited number
+	// of tick marks, we can use a simple loop to determine the min te
+	stepsBack := 0
+	for (float64(stepsBack-1) * te) > pw.miny {
+		stepsBack--
+	}
+
+	for {
+		y := float64(stepsBack) * te
+		if y >= pw.maxy {
+			break
+		}
+		if stepsBack != 0 {
+			pw.drawVerticalTick(wx, y)
+		}
+		stepsBack++
+	}
+}
+
+func (pw *PlotWindow) drawVerticalTick(wx int, y float64) {
+	ww := pw.txtw.Width()
+	wy, _ := pw.transformY(y)
+	pw.txtw.SetXY(wx, wy)
+	window.PutByte(pw.txtw, '+')
+	if (wx + 10) < ww {
+		pw.txtw.SetXY(wx+3, wy)
+		window.Print(pw.txtw, fmt.Sprintf("%.2f", y))
+	}
+}
+
+// use a nice-looking scale. 1, 0.5, 0.25, 0.1, 0.05, 0.025, 0.01, etc
+func searchScaleDownward(cpu, minSpacing float64) float64 {
+	tens := 1.0
+	partial := 1
+	te := 1.0
+
+	for {
+		switch partial {
+		case 1:
+			partial = 2
+		case 2:
+			partial = 4
+		case 4:
+			partial = 1
+			tens *= 10
+		}
+
+		newte := 1.0 / (tens * float64(partial))
+		if (cpu * newte) < minSpacing {
+			// too far
+			break
+		}
+		te = newte
+	}
+
+	return te
+}
+
+func searchScaleUpward(cpu, maxSpacing float64) float64 {
+	tens := 1.0
+	partialDeci := 10
+	te := 1.0
+
+	for {
+		switch partialDeci {
+		case 10:
+			partialDeci = 25
+		case 25:
+			partialDeci = 50
+		case 50:
+			partialDeci = 10
+			tens *= 10
+		}
+
+		newte := tens * float64(partialDeci) / 10.0
+		if (cpu * newte) > maxSpacing {
+			// too far
+			break
+		}
+		te = newte
+	}
+
+	return te
+}
+
+func (pw *PlotWindow) drawHorizontalAxis(y int) error {
+	w := pw.txtw.Width()
+	pw.txtw.SetXY(0, y)
+	for x := 0; x < w; x++ {
+		if err := pw.txtw.Write('-'); err != nil {
+			return err
+		}
+	}
+	pw.drawHorizontalTickMarks(y)
+	return nil
+}
+
+const minHorizontalSpacing = 9.0
+const maxHorizontalSpacing = 18.0
+
+func (pw *PlotWindow) drawHorizontalTickMarks(wy int) {
+	ww := pw.txtw.Width()   // width in characters
+	xr := pw.maxx - pw.minx // units
+	cpu := float64(ww) / xr // characters / unit
+	var te float64 = 1      // ticks every (0.5, 1, etc)
+	if cpu > maxHorizontalSpacing {
+		te = searchScaleDownward(cpu, minHorizontalSpacing)
+	} else if cpu < minHorizontalSpacing {
+		te = searchScaleUpward(cpu, maxHorizontalSpacing)
+	}
+
+	stepsBack := 0
+	for (float64(stepsBack-1) * te) > pw.minx {
+		stepsBack--
+	}
+
+	for {
+		x := float64(stepsBack) * te
+		if x >= pw.maxx {
+			break
+		}
+		if stepsBack != 0 {
+			pw.drawHorizontalTick(x, wy)
+		}
+		stepsBack++
+	}
+}
+
+const horizontalNumberPad = 5
+
+func (pw *PlotWindow) drawHorizontalTick(x float64, wy int) {
+	ww, wh := pw.txtw.Size()
+	wx, _ := pw.transformX(x)
+	if wy >= 0 {
+		pw.txtw.SetXY(wx, wy)
+		window.PutByte(pw.txtw, '+')
+	}
+	if (wx > horizontalNumberPad) && (wx < (ww - horizontalNumberPad)) && (wy+1) < wh {
+		s := fmt.Sprintf("%.2f", x)
+		pw.txtw.SetXY(wx-len(s)/2, wy+1)
+		window.Print(pw.txtw, s)
+	}
+}
---
+++ io/window/plotwin/update.go
@@ -0,0 +1,142 @@
+package plotwin
+
+import (
+	"fmt"
+	"mattwach/rpngo/rpn"
+)
+
+func (pw *PlotWindow) Update(r *rpn.RPN) error {
+	points, err := pw.createPoints(r)
+	if err != nil {
+		return err
+	}
+	pw.txtw.Erase()
+	defer pw.txtw.Refresh()
+	if pw.autox {
+		pw.adjustAutoX(points)
+	}
+	if pw.autoy {
+		pw.adjustAutoY(points)
+	}
+	if err := pw.drawAxis(); err != nil {
+		return err
+	}
+	if err := pw.plotPoints(points); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pw *PlotWindow) createPoints(r *rpn.RPN) ([]Point, error) {
+	var points []Point
+	for _, plot := range pw.plots {
+		var err error
+		points, err = pw.addPoints(r, points, plot)
+		if err != nil {
+			pw.plots = nil
+			return nil, fmt.Errorf("plot error for %v, removed all plots: %v", plot.fn, err)
+		}
+	}
+	return points, nil
+}
+
+func (pw *PlotWindow) adjustAutoX(points []Point) {
+	if len(points) == 0 {
+		pw.minx = 0
+		pw.maxx = 0
+	} else {
+		pw.minx = points[0].x
+		pw.maxx = points[0].x
+	}
+	for _, p := range points {
+		if p.x < pw.minx {
+			pw.minx = p.x
+		} else if p.x > pw.maxx {
+			pw.maxx = p.x
+		}
+	}
+	if pw.minx == pw.maxx {
+		// create a little spread to avoid math issues
+		pw.minx -= 1.0
+		pw.maxx += 1.0
+	}
+}
+
+func (pw *PlotWindow) adjustAutoY(points []Point) {
+	if len(points) == 0 {
+		pw.miny = 0
+		pw.maxy = 0
+	} else {
+		pw.miny = points[0].y
+		pw.maxy = points[0].y
+	}
+	for _, p := range points {
+		if p.y < pw.miny {
+			pw.miny = p.y
+		} else if p.y > pw.maxy {
+			pw.maxy = p.y
+		}
+	}
+	if pw.miny == pw.maxy {
+		// create a little spread to avoid math issues
+		pw.miny -= 1.0
+		pw.maxy += 1.0
+	}
+	// open up the y a bit (20% or so)
+	delta := (pw.maxy - pw.miny) / 10
+	pw.maxy += delta
+	pw.miny -= delta
+}
+
+func (pw *PlotWindow) plotPoints(points []Point) error {
+	var color uint16
+	for _, p := range points {
+		if p.color != color {
+			color = p.color
+			pw.txtw.Color(int(color>>10), int((color>>5)&31), int(color&31), 0, 0, 0)
+		}
+		x, xok := pw.transformX(p.x)
+		if !xok {
+			continue
+		}
+		y, yok := pw.transformY(p.y)
+		if !yok {
+			continue
+		}
+		pw.txtw.SetXY(x, y)
+		if err := pw.txtw.Write('*'); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (pw *PlotWindow) transformX(x float64) (int, bool) {
+	x -= pw.minx
+	if x < 0 {
+		// off the left of the screen
+		return 0, false
+	}
+	// convert x to a ratio between 0 and 1
+	x = x / (pw.maxx - pw.minx)
+	if x >= 1 {
+		// off the right of the screen
+		return 0, false
+	}
+	return int(float64(pw.txtw.Width()) * x), true
+}
+
+func (pw *PlotWindow) transformY(y float64) (int, bool) {
+	y -= pw.miny
+	if y < 0 {
+		// off the top of the screen
+		return 0, false
+	}
+	// convert y to a ratio between 0 and 1
+	y = y / (pw.maxy - pw.miny)
+	if y >= 1 {
+		// off the bottom of the screen
+		return 0, false
+	}
+	return pw.txtw.Height() - int(float64(pw.txtw.Height())*y) - 1, true
+}
