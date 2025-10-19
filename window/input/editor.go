cdbd68c7a266a1dfe9ded84bd36757318cc90128
Matt Wachowski <matt.wachowski@gmail.com>
2025-10-19 14:39:16 -0700

    Update fframe API so that editor.go will compile.

--- window/input/editor.go
+++ window/input/editor.go
@@ -41,7 +41,7 @@ func (iw *InputWindow) Edit(r *rpn.RPN) error {
 		case 27: // ESC
 			tw, th := iw.txtb.Txtw.TextSize()
 			iw.txtb.RefreshArea(0, 0, tw, th)
-			return r.PushString(string(ed.buff))
+			return r.PushFrame(rpn.StringFrame(string(ed.buff)))
 		}
 	}
 }
