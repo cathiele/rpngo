86cab700724466c42ea5a70503569665d885cca1
Matt Wachowski <matt.wachowski@gmail.com>
2025-10-30 17:04:06 -0700

    Developing filter, filtern, etc...

--- functions/filter.go
+++ functions/filter.go
@@ -12,16 +12,108 @@ const FilterHelp = "Copies each value from the bottom to the top of the stack " 
 	"Examples:\n" +
 	"{2 *} filter  # multiply every stack value by 2\n" +
 	"{$0 100 >= {0/} if} filter  # keep only values < 100\n" +
-	"0 {+} filter  # sum all values\n" +
-	"$0 min= {$0 $min < {min=} {0/} ifelse} filter $min  # find minimum\n"
+	"\n" +
+	"See Also: filterm, filtern, filtermn"
 
 func Filter(r *rpn.RPN) error {
+	// -1 is to account for the macro term
+	return filterMN(r, len(r.Frames)-1, 0)
+}
+
+const FilterMHelp = "This function works like filter, but selects the top m elements " +
+        "on the stack instead of working on every element (m does not include filterm arguments)" +
+	"\n" +
+	"Examples:\n" +
+	"{2 *} 5 filterm  # multiply the top 5 stack values by 2\n" +
+	"{$0 100 >= {0/} if} 10 filterm  # filter the top 10 stack values\n" +
+	"\n" +
+	"See Also: filter, filtern, filtermn"
+
+func FilterM(r *rpn.RPN) error {
+	f, err := r.PopFrame()
+	if err != nil {
+		return err
+	}
+	m, err := f.Int()
+	if err != nil {
+		return err
+	}
+	return filterMN(r, int(m), 0)
+}
+
+const FilterNHelp = "This function works like filter, but ends at element head-n (not including " +
+        "filtern arguments)\n" +
+        "This is useful for preserving \"working values\"" +
+	"\n" +
+	"Examples:\n" +
+	"0 {+} 1 filtern # sum all values\n" +
+	"$0 {$1 $1 < {0/} {1/} ifelse} 1 filtern  # minimum\n" +
+	"\n" +
+	"See Also: filter, filterm, filtermn"
+
+func FilterN(r *rpn.RPN) error {
+	f, err := r.PopFrame()
+	if err != nil {
+		return err
+	}
+	n, err := f.Int()
+	if err != nil {
+		return err
+	}
+	// -1 is to account for the macro term
+	return filterMN(r, len(r.Frames)-1, int(n))
+}
+
+const FilterMNHelp = "This function works like filterm and filtern combined, filtering\n" +
+	"from m elements back to head-n (not including filtermn arguments)\n" +
+	"\n" +
+	"Examples:\n" +
+	"0 {+} 5 1 filtermn # sum top 5 values\n" +
+	"$0 {$1 $1 < {0/} {1/} ifelse} 5 1 filtermn  # minimum of top 5 values\n" +
+	"\n" +
+	"See Also: filter, filterm, filtern"
+
+func FilterMN(r *rpn.RPN) error {
+	f, err := r.PopFrame()
+	if err != nil {
+		return err
+	}
+	n, err := f.Int()
+	if err != nil {
+		return err
+	}
+	f, err = r.PopFrame()
+	if err != nil {
+		return err
+	}
+	m, err := f.Int()
+	if err != nil {
+		return err
+	}
+	return filterMN(r, int(m), int(n))
+}
+
+// Common execution function
+func filterMN(r *rpn.RPN, m, n int) error {
 	fn, err := r.PopFrame()
 	if err != nil {
 		return err
 	}
-	ssize := len(r.Frames)
-	if ssize <= 0 {
+	if m == 0 {
+		return nil
+	}
+	startIdx := len(r.Frames) - m - 1
+	if startIdx < 0 {
+		return rpn.ErrNotEnoughStackFrames
+	}
+	endIdx := len(r.Frames) - n
+	if endIdx < 0 {
+		return rpn.ErrNotEnoughStackFrames
+	}
+	if endIdx < startIdx {
+		return rpn.ErrIllegalValue
+	}
+	if endIdx == startIdx {
 		return nil
 	}
 	fields := make([]string, 0, 16)
@@ -32,7 +124,7 @@ func Filter(r *rpn.RPN) error {
 	if err := parse.Fields(fn.String(false), addField); err != nil {
 		return err
 	}
-	for i := 0; i < ssize; i++ {
+	for i := startIdx; i < endIdx; i++ {
 		if i >= len(r.Frames) {
 			return rpn.ErrNotEnoughStackFrames
 		}
@@ -43,11 +135,11 @@ func Filter(r *rpn.RPN) error {
 			return err
 		}
 	}
-	newSize := len(r.Frames) - ssize
+	newSize := len(r.Frames) - (endIdx - startIdx)
 	if newSize <= 0 {
 		r.Frames = r.Frames[:0]
 	} else {
-		copy(r.Frames, r.Frames[ssize:])
+		copy(r.Frames[startIdx:], r.Frames[endIdx:])
 		r.Frames = r.Frames[:newSize]
 	}
 	return nil
--- functions/filter_test.go
+++ functions/filter_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestFilter(t *testing.T) {
 	data := []rpn.UnitTestExecData{
+/*
 		{
 			Args:    []string{"filter"},
 			WantErr: rpn.ErrStackEmpty,
@@ -37,6 +38,88 @@ func TestFilter(t *testing.T) {
 			Args:    []string{"1", "2", "3", "{0/ 0/}", "filter"},
 			Want:    []string{"1"},
 			WantErr: rpn.ErrNotEnoughStackFrames,
+		},
+		{
+			Args: []string{"1", "2", "3", "4", "{sq}", "2", "filterm"},
+			Want: []string{"1", "2", "9", "16"},
+		},
+		{
+			Args: []string{"10", "100", "30", "200", "{$0 100 >= {0/} if}", "2", "filterm"},
+			Want: []string{"10", "100", "30"},
+		},
+		{
+			Args: []string{"filterm"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"0", "filterm"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"1", "filterm"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"{sq}", "0", "filterm"},
+		},
+		{
+			Args: []string{"{sq}", "1", "filterm"},
+			WantErr: rpn.ErrNotEnoughStackFrames,
+		},
+		{
+			Args: []string{"1", "2", "3", "0", "{+}", "1", "filtern"},
+			Want: []string{"6"},
+		},
+		{
+			Args: []string{"10", "2", "30", "5", "$0", "{$1 $1 < {0/} {1/} ifelse}", "1", "filtern"},
+			Want: []string{"2"},
+		},
+		{
+			Args: []string{"filtern"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"0", "filtern"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"1", "filtern"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"{sq}", "0", "filtern"},
+		},
+		{
+			Args: []string{"{sq}", "1", "filtern"},
+			WantErr: rpn.ErrNotEnoughStackFrames,
+		},
+*/
+		{
+			Args: []string{"4", "5", "6", "7", "{sq}", "2", "1", "filtermn"},
+			Want: []string{"4", "7", "25", "36"},
+		},
+		{
+			Args: []string{"10", "2", "30", "5", "0", "{+}", "3", "1", "filtermn"},
+			Want: []string{"10", "37"},
+		},
+		{
+			Args: []string{"10", "2", "30", "5", "$0", "{$1 $1 < {0/} {1/} ifelse}", "3", "1", "filtermn"},
+			Want: []string{"10", "2"},
+		},
+		{
+			Args: []string{"filtermn"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"0", "filtermn"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"0", "1", "filtermn"},
+			WantErr: rpn.ErrStackEmpty,
+		},
+		{
+			Args: []string{"{sq}", "0", "0", "filtermn"},
 		},
 	}
 	rpn.UnitTestExecAll(t, data, func(r *rpn.RPN) { RegisterAll(r) })
